29d90d5fd8b6ae3b3105c01d738fc7cb31f09234
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-03-05 23:32:27 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (48.12%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.py
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.py
@@ -1,8 +1,3 @@
 class Solution:
     def coloredCells(self, n: int) -> int:
-        sum2=0
-        n1=n*2-1-2
-        while(n1>0):
-            sum2+=n1
-            n1-=2
-        return (2*sum2)+(n*2-1)
+        return 2*(n-1)*n+1

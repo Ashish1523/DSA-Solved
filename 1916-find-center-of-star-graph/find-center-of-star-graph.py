8887cd89e419220fd32d8434a1ca9c93d0468656
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-03-10 23:10:55 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (45.51%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.py
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.py
@@ -1,10 +1,6 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        hashmap=defaultdict(int)
-        for i in range(len(edges)):
-            for j in range(len(edges[0])):
-                hashmap[edges[i][j]]+=1
-        for i in hashmap:
-            if hashmap[i]==len(edges):
-                return i
-        # print(hashmap)
+        if edges[0][0] in edges[1]:
+            return edges[0][0]
+        else:
+            return edges[0][1]

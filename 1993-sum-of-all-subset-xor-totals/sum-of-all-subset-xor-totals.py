29e84700a9dd9591a70c41751551ac34ad4d0351
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-04-05 13:06:39 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (23.27%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
@@ -1,21 +1,25 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        result=[]
-        subset=[]
-        def dfs(i):
-            if i>=len(nums):
-                result.append(subset.copy())
-                return
-            subset.append(nums[i])
-            dfs(i+1)
+        # result=[]
+        # subset=[]
+        # def dfs(i):
+        #     if i>=len(nums):
+        #         result.append(subset.copy())
+        #         return
+        #     subset.append(nums[i])
+        #     dfs(i+1)
 
-            subset.pop()
-            dfs(i+1)
-        dfs(0)
-        f_res=0
-        for i in result:
-            s=0
-            for j in i:
-                s^=j
-            f_res+=s
-        return f_res
+        #     subset.pop()
+        #     dfs(i+1)
+        # dfs(0)
+        # f_res=0
+        # for i in result:
+        #     s=0
+        #     for j in i:
+        #         s^=j
+        #     f_res+=s
+        # return f_res
+        ans = 0
+        for i in nums:
+            ans |= i
+        return ans << (len(nums)-1)

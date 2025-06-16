8632d35c4edc57b45ffb4cdb1ae0018dc0013c25
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-06-16 17:32:42 +0530

    Time: 6 ms (47.10%) | Memory: 19 MB (35.09%) - LeetSync

---
+++ 800-letter-case-permutation/letter-case-permutation.py
@@ -0,0 +1,22 @@
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        res=[]
+        stack=[]
+        def backtrack(i):
+            if i>=len(s) or i<0:
+                res.append("".join(stack))
+                return
+            if s[i].isalpha():
+                stack.append(s[i].lower())
+                backtrack(i+1)
+                stack.pop()
+
+                stack.append(s[i].upper())
+                backtrack(i+1)
+                stack.pop()
+            else:
+                stack.append(s[i])
+                backtrack(i+1)
+                stack.pop()
+        backtrack(0)
+        return res

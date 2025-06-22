01495a5cfb49a700a3d27c3ae78cbf74eb35070e
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-06-22 15:01:17 +0530

    Time: 4 ms (71.09%) | Memory: 21.3 MB (90.36%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -3,28 +3,37 @@ class MinStack:
     def __init__(self):
         self.s=[]
         # self.n=len(self.s)
-        # self.min=float("inf")
+        self.min=float("inf")
     def push(self, val: int) -> None:
         if len(self.s)==0:
-            self.s.append([val,val])
+            self.min=val
+            self.s.append(val)
         else:
-            self.s.append([val,min(val,self.s[len(self.s)-1][1])])
+            if val< self.min:
+                self.s.append(2*val-self.min)
+                self.min=val
+            else:
+                self.s.append(val)
         # print(self.s)
 
     def pop(self) -> None:
-        self.s.pop()
+        k=self.s.pop()
+        if k<self.min:
+            self.min=2*self.min-k
+        
         
 
     def top(self) -> int:
         # print(self.s)
-        if len(self.s)>0:
-            return self.s[len(self.s)-1][0]
+        if self.s[len(self.s)-1]<self.min:
+            return self.min
         else:
-            return -1
+            return self.s[len(self.s)-1]
+
 
     def getMin(self) -> int:
         # print(self.s)
-        return self.s[len(self.s)-1][1]
+        return self.min
 
 
 # Your MinStack object will be instantiated and called as such:

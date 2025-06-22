f809e8e2f284528f9fb6dc9bfe4cd5cfe8028495
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-06-22 20:24:49 +0530

    Time: 1 ms (52.89%) | Memory: 17.9 MB (89.87%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,18 +1,17 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums1Idx={n:i for i,n in enumerate(nums1)}
-        res=[-1]*len(nums1)
-
-        stack=[]
-        for i in range(len(nums2)):
-            cur=nums2[i]
-            while stack and cur >stack[-1]:
-                val=stack.pop()
-                idx=nums1Idx[val]
-                res[idx]=cur
-            if cur in nums1Idx:
-                stack.append(cur)
-        return res
+        # nums1Idx={n:i for i,n in enumerate(nums1)}
+        # res=[-1]*len(nums1)
+        # stack=[]
+        # for i in range(len(nums2)):
+        #     cur=nums2[i]
+        #     while stack and cur >stack[-1]:
+        #         val=stack.pop()
+        #         idx=nums1Idx[val]
+        #         res[idx]=cur
+        #     if cur in nums1Idx:
+        #         stack.append(cur)
+        # return res
 
 
         # h=defaultdict(int)
@@ -33,3 +32,21 @@ class Solution:
         # for i in nums1:
         #     res.append(h[i])
         # return res
+
+        valIdx={}
+        for i in range(len(nums1)):
+            valIdx[nums1[i]]=i
+        stack=[]
+        res=[-1]*len(nums1)
+        for i in range(len(nums2)-1,-1,-1):
+            while stack and stack[-1]<=nums2[i]:
+                stack.pop()
+            # print(stack)
+            if len(stack)==0:
+                if nums2[i] in valIdx:
+                    res[valIdx[nums2[i]]]=-1
+            else:
+                if nums2[i] in valIdx:
+                    res[valIdx[nums2[i]]]=stack[-1]
+            stack.append(nums2[i])
+        return res

a4503c82b8ec80474cbd11d95759b0e60838c441
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-06-25 17:22:40 +0530

    Time: 105 ms (36.68%) | Memory: 21.8 MB (49.84%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
@@ -1,24 +1,40 @@
+from typing import List
+
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
-        MOD=10**9+7
-        res=0
-        stack=[]
+        MOD = 10**9 + 7
+        n = len(arr)
 
-        for i,n in enumerate(arr):
-            while stack and n<stack[-1][1]:
-                j,m=stack.pop()
-                left=j-stack[-1][0] if stack else j+1
-                right=i-j
-                res=(res+m*left*right)%MOD
-            stack.append((i,n))
-        
-        for i in range(len(stack)):
-            j,n=stack[i]
-            left=j-stack[i-1][0] if i>0 else j+1
-            right=len(arr)-j
-            res=(res+n*left*right)%MOD
+        # Previous Less Element (strictly less)
+        def prevLess(arr):
+            stack = []
+            prev = [-1] * n
+            for i in range(n):
+                while stack and arr[stack[-1]] > arr[i]:
+                    stack.pop()
+                prev[i] = stack[-1] if stack else -1
+                stack.append(i)
+            return prev
 
-        return res
+        # Next Less or Equal Element (to avoid double counting)
+        def nextLessEqual(arr):
+            stack = []
+            next_ = [n] * n
+            for i in range(n - 1, -1, -1):
+                while stack and arr[stack[-1]] >= arr[i]:
+                    stack.pop()
+                next_[i] = stack[-1] if stack else n
+                stack.append(i)
+            return next_
 
+        prev = prevLess(arr)
+        next_ = nextLessEqual(arr)
 
-        
+        result = 0
+        for i in range(n):
+            left = i - prev[i]
+            right = next_[i] - i
+            result += arr[i] * left * right
+            result %= MOD
+
+        return result

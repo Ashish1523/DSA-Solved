286be9d7ba7174e3318156fb63036e57ca2065d9
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-06-28 10:06:06 +0530

    Time: 150 ms (44.50%) | Memory: 78.2 MB (77.21%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,25 +1,52 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.q=deque()
-        self.map=dict()
+        self.map={}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
         self.capacity=capacity
+
+    def _remove(self, node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+    
+    def _add_to_front(self, node):
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
 
     def get(self, key: int) -> int:
         if key not in self.map:
             return -1
-        self.q.remove(key)
-        self.q.append(key)
-        return self.map[key]
+        node = self.map[key]
+        self._remove(node)
+        self._add_to_front(node)
+        return node.value
 
     def put(self, key: int, value: int) -> None:
         if key in self.map:
-            self.q.remove(key)
-        elif len(self.q)==self.capacity:
-            lru=self.q.popleft()
-            del self.map[lru]
-        self.q.append(key)
-        self.map[key]=value
+            self._remove(self.map[key])
+        node = Node(key, value)
+        self._add_to_front(node)
+        self.map[key] = node
+
+        if len(self.map) > self.capacity:
+            # Remove the least recently used node
+            lru = self.tail.prev
+            self._remove(lru)
+            del self.map[lru.key]
 
 
 

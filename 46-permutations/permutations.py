830ca9b8590f43c0e208e90034258116d17a2f8e
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-06-29 20:30:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (95.33%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,19 +1,31 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        # res=[]
+        # stack=[]
+        # used=[False]*len(nums)
+        # def com():
+        #     if len(stack)==len(nums):
+        #         res.append(stack[:])
+        #         return
+        #     for i in range(len(nums)):
+        #         if used[i]:
+        #             continue
+        #         used[i]=True
+        #         stack.append(nums[i])
+        #         com()
+        #         stack.pop()
+        #         used[i]=False
+        # com()
+        # return res
+
         res=[]
-        stack=[]
-        used=[False]*len(nums)
-        def com():
-            if len(stack)==len(nums):
-                res.append(stack[:])
+        def per(k):
+            if k==len(nums):
+                res.append(nums[:])
                 return
-            for i in range(len(nums)):
-                if used[i]:
-                    continue
-                used[i]=True
-                stack.append(nums[i])
-                com()
-                stack.pop()
-                used[i]=False
-        com()
+            for i in range(k,len(nums)):
+                nums[k],nums[i]=nums[i],nums[k]
+                per(k+1)
+                nums[k],nums[i]=nums[i],nums[k]
+        per(0)
         return res

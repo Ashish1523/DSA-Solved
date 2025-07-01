309d38637fc06ba4ca7726b5d050e61fe0c8d62e
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-07-01 22:30:50 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (54.73%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -16,23 +16,41 @@ class Solution:
         # post(root)
         # return ans
 
-        if not root:
-            return []
+        # if not root:
+        #     return []
         
-        stack1=[root]
-        stack2=[]
-        result=[]
+        # stack1=[root]
+        # stack2=[]
+        # result=[]
 
-        while stack1:
-            node=stack1.pop()
-            stack2.append(node)
+        # while stack1:
+        #     node=stack1.pop()
+        #     stack2.append(node)
 
-            if node.left:
-                stack1.append(node.left)
-            if node.right:
-                stack1.append(node.right)
+        #     if node.left:
+        #         stack1.append(node.left)
+        #     if node.right:
+        #         stack1.append(node.right)
             
-        while stack2:
-            result.append(stack2.pop().val)
+        # while stack2:
+        #     result.append(stack2.pop().val)
             
-        return result
+        # return result
+
+        res=[]
+        st=[]
+        curr=root
+        last=None
+
+        while curr or st:
+            if curr:
+                st.append(curr)
+                curr=curr.left
+            else:
+                peek=st[-1]
+                if peek.right and last!=peek.right:
+                    curr=peek.right
+                else:
+                    res.append(peek.val)
+                    last=st.pop()
+        return res

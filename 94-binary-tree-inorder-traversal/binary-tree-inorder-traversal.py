dc99fd0407fbe39390ef48e4b79c455070f7d225
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-07-11 20:43:23 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (84.24%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -17,15 +17,38 @@ class Solution:
         # inord(root)
         # return t1
 
-        stack=[]
-        res=[]
-        curr=root
+        # stack=[]
+        # res=[]
+        # curr=root
 
-        while curr or stack:
-            while curr:
-                stack.append(curr)
-                curr=curr.left
-            curr=stack.pop()
-            res.append(curr.val)
-            curr=curr.right
-        return res
+        # while curr or stack:
+        #     while curr:
+        #         stack.append(curr)
+        #         curr=curr.left
+        #     curr=stack.pop()
+        #     res.append(curr.val)
+        #     curr=curr.right
+        # return res
+
+        #Morris Traversal
+        inorder=[]
+        if not root:
+            return []
+        curr=root
+        while curr!=None:
+            if curr.left==None:
+                inorder.append(curr.val)
+                curr=curr.right
+            else:
+                prev=curr.left
+                while(prev.right and prev.right !=curr):
+                    prev=prev.right
+                if prev.right==None:
+                    prev.right=curr
+                    curr=curr.left
+                else:
+                    prev.right=None
+                    inorder.append(curr.val)
+                    curr=curr.right
+        
+        return inorder

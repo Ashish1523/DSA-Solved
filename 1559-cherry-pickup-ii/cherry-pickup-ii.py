8c8312dcf473f599b930637d50188e961bee613f
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-07-25 20:48:58 +0530

    Time: 1197 ms (50.11%) | Memory: 29 MB (39.32%) - LeetSync

---
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def cherryPickup(self, grid: List[List[int]]) -> int:
+        row=len(grid)
+        col=len(grid[0])
+        dp=[[[-1 for _ in range(col)] for _ in range(col)] for _ in range(row)]
+        def dfs(r,c1,c2):
+            if c1<0 or c1>=col or c2<0 or c2>=col:
+                return float('-inf')
+            if r==row-1:
+                if c1==c2:
+                    return grid[r][c1]
+                return grid[r][c1]+grid[r][c2]
+            if dp[r][c1][c2]!=-1:
+                return dp[r][c1][c2]
+            maxi=float('-inf')
+            for i in range(-1,2):
+                for j in range(-1,2):
+                    value=0
+                    if c1==c2:
+                        value=grid[r][c1]
+                    else:
+                        value=grid[r][c1]+grid[r][c2]
+                    value+=dfs(r+1,c1+i,c2+j)
+                    maxi=max(value,maxi)
+            dp[r][c1][c2]=maxi
+            return dp[r][c1][c2]
+        # print
+        return dfs(0,0,col-1)
+

b13a9fb239ec9d9bcfa79906e3cda1120687cb92
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-07-25 15:49:56 +0530

    Time: 6 ms (38.68%) | Memory: 18.7 MB (82.92%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -8,10 +8,14 @@ class Solution:
         #     right=dfs(i+1,j+1)
         #     return min(left,right)+triangle[i][j]
         # return dfs(0,0)
-        dp=triangle
+        front=[0 for _ in range(n)]
+        for j in range(n):
+            front[j]=triangle[n-1][j]
+        curr=front
         for i in range(n-2,-1,-1):
             for j in range(len(triangle[i])):
-                bottom=dp[i+1][j]
-                bottom_left=dp[i+1][j+1]
-                dp[i][j]=min(bottom,bottom_left)+triangle[i][j]
-        return dp[0][0]
+                bottom=front[j]
+                bottom_left=front[j+1]
+                curr[j]=min(bottom,bottom_left)+triangle[i][j]
+            front=curr
+        return front[0]

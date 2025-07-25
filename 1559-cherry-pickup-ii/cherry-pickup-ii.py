6baee63b63113d6df5c844141b848897b926b309
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-07-25 22:20:07 +0530

    Time: 2486 ms (5.08%) | Memory: 25.5 MB (54.98%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.py
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.py
@@ -3,27 +3,52 @@ class Solution:
         row=len(grid)
         col=len(grid[0])
         dp=[[[-1 for _ in range(col)] for _ in range(col)] for _ in range(row)]
-        def dfs(r,c1,c2):
-            if c1<0 or c1>=col or c2<0 or c2>=col:
-                return float('-inf')
-            if r==row-1:
+        # def dfs(r,c1,c2):
+        #     if c1<0 or c1>=col or c2<0 or c2>=col:
+        #         return float('-inf')
+        #     if r==row-1:
+        #         if c1==c2:
+        #             return grid[r][c1]
+        #         return grid[r][c1]+grid[r][c2]
+        #     if dp[r][c1][c2]!=-1:
+        #         return dp[r][c1][c2]
+        #     maxi=float('-inf')
+        #     for i in range(-1,2):
+        #         for j in range(-1,2):
+        #             value=0
+        #             if c1==c2:
+        #                 value=grid[r][c1]
+        #             else:
+        #                 value=grid[r][c1]+grid[r][c2]
+        #             value+=dfs(r+1,c1+i,c2+j)
+        #             maxi=max(value,maxi)
+        #     dp[r][c1][c2]=maxi
+        #     return dp[r][c1][c2]
+
+        # return dfs(0,0,col-1)
+
+        for c1 in range(col):
+            for c2 in range(col):
                 if c1==c2:
-                    return grid[r][c1]
-                return grid[r][c1]+grid[r][c2]
-            if dp[r][c1][c2]!=-1:
-                return dp[r][c1][c2]
-            maxi=float('-inf')
-            for i in range(-1,2):
-                for j in range(-1,2):
-                    value=0
-                    if c1==c2:
-                        value=grid[r][c1]
-                    else:
-                        value=grid[r][c1]+grid[r][c2]
-                    value+=dfs(r+1,c1+i,c2+j)
-                    maxi=max(value,maxi)
-            dp[r][c1][c2]=maxi
-            return dp[r][c1][c2]
-        # print
-        return dfs(0,0,col-1)
+                    dp[row-1][c1][c2]=grid[row-1][c1]
+                else:
+                    dp[row-1][c1][c2]=grid[row-1][c1]+grid[row-1][c2]
+        
+        for r in range(row-2,-1,-1):
+            for c1 in range(col):
+                for c2 in range(col):
+                    maxi=float('-inf')
+                    for i in range(-1,2):
+                        for j in range(-1,2):
+                            value=0
+                            if c1==c2:
+                                value=grid[r][c1]
+                            else:
+                                value=grid[r][c1]+grid[r][c2]
+                            value+=dp[r+1][c1+i][c2+j] if 0<=c1+i<col and 0<=c2+j<col else float('-inf')
+                            maxi=max(value,maxi)
+                    dp[r][c1][c2]=maxi
+
+        return dp[0][0][col-1]
+                            
 

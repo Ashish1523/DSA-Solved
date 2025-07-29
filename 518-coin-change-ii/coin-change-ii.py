ffd75a68c6fa9e5f97e53a2e02df441c1e5fd914
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-07-29 10:04:55 +0530

    Time: 279 ms (49.88%) | Memory: 40.1 MB (46.26%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -3,13 +3,15 @@ class Solution:
         n=len(coins)
         dp=[[-1 for _ in range(amount+1)] for _ in range(n)]
         def dfs(i,amt):
-            if i<0 or amt<0:
+            if i<0:
                 if amt==0:
                     return 1
                 return 0
             if dp[i][amt]!=-1:
                 return dp[i][amt]
-            pick=dfs(i,amt-coins[i])
+            pick=0
+            if coins[i]<=amt:
+                pick=dfs(i,amt-coins[i])
             notPick=dfs(i-1,amt)
             dp[i][amt]=pick+notPick
             return dp[i][amt]

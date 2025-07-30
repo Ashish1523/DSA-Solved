b1fdd4525dbe8986ba0e3755100a53237d70cf7f
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-07-30 20:24:46 +0530

    Time: 476 ms (40.93%) | Memory: 44.2 MB (68.49%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -4,16 +4,16 @@ class Solution:
         n=len(t)
         dp=[[-1 for _ in range(n+1)] for _ in range(m+1)]
         def dfs(i,j):
-            if j<0:
+            if j==0:
                 return 1
-            if i<0:
+            if i==0:
                 return 0
             if dp[i][j]!=-1:
                 return dp[i][j]
-            if s[i]==t[j]:
+            if s[i-1]==t[j-1]:
                 dp[i][j]=dfs(i-1,j-1)+dfs(i-1,j)
                 return dp[i][j]
             dp[i][j]=dfs(i-1,j)
             return dp[i][j]
         
-        return dfs(m-1,n-1)
+        return dfs(m,n)

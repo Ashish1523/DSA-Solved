ad62806b498093a2817110799ff7bfbc2db204d3
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-07-30 21:18:20 +0530

    Time: 268 ms (86.04%) | Memory: 18 MB (91.28%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -2,18 +2,31 @@ class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         m=len(s)
         n=len(t)
-        dp=[[-1 for _ in range(n+1)] for _ in range(m+1)]
-        def dfs(i,j):
-            if j==0:
-                return 1
-            if i==0:
-                return 0
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            if s[i-1]==t[j-1]:
-                dp[i][j]=dfs(i-1,j-1)+dfs(i-1,j)
-                return dp[i][j]
-            dp[i][j]=dfs(i-1,j)
-            return dp[i][j]
+        # dp=[[-1 for _ in range(n+1)] for _ in range(m+1)]
+        # def dfs(i,j):
+        #     if j==0:
+        #         return 1
+        #     if i==0:
+        #         return 0
+        #     if dp[i][j]!=-1:
+        #         return dp[i][j]
+        #     if s[i-1]==t[j-1]:
+        #         dp[i][j]=dfs(i-1,j-1)+dfs(i-1,j)
+        #         return dp[i][j]
+        #     dp[i][j]=dfs(i-1,j)
+        #     return dp[i][j]
         
-        return dfs(m,n)
+        # return dfs(m,n)
+
+        dp=[0 for _ in range(n+1)]
+        dp[0]=1
+        
+        for i in range(1,m+1):
+            prev=dp[:]
+            for j in range(n,0,-1):
+                if s[i-1]==t[j-1]:
+                    dp[j]=prev[j-1]+prev[j]
+                else:
+                    dp[j]=prev[j]
+            # prev=dp
+        return dp[n]

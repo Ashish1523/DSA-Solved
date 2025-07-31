d13e3dc49705d52f56a15e5f99bc3907aa70fbf2
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-07-31 19:14:02 +0530

    Time: 321 ms (71.48%) | Memory: 18.1 MB (81.45%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.py
+++ 44-wildcard-matching/wildcard-matching.py
@@ -25,20 +25,22 @@ class Solution:
         #     return dp[ind1][ind2]
         # return dfs(s1,s2)
 
-        dp=[[False for _ in range(s2+1)] for _ in range(s1+1)]
-        dp[0][0]=True
+        dp=[False for _ in range(s2+1)]
+        dp[0]=True
         for j in range(1, s2+1):
             if p[j-1] == '*':
-                dp[0][j] = dp[0][j-1]
+                dp[j] = dp[j-1]
             else:
                 break
 
         for ind1 in range(1,s1+1):
+            prev=dp[:]
+            dp[0]=False
             for ind2 in range(1,s2+1):
                 if s[ind1-1]==p[ind2-1] or p[ind2-1]=="?":
-                    dp[ind1][ind2]=dp[ind1-1][ind2-1]
+                    dp[ind2]=prev[ind2-1]
                 elif p[ind2-1]=="*":
-                    dp[ind1][ind2]=dp[ind1-1][ind2] or dp[ind1][ind2-1]
+                    dp[ind2]=prev[ind2] or dp[ind2-1]
                 else:
-                    dp[ind1][ind2]=False
-        return dp[s1][s2]
+                    dp[ind2]=False
+        return dp[s2]

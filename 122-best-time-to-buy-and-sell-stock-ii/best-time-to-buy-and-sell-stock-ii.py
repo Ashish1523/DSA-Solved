4c121ba7664547661afd2939d18e18676dde2352
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-07-31 20:51:14 +0530

    Time: 7 ms (13.26%) | Memory: 18.5 MB (99.79%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -19,13 +19,14 @@ class Solution:
         
         # return dfs(0,1)
 
-        dp=[[0 for _ in range(2)] for _ in range(n+1)]
+        dp=[0,0]
 
         for ind in range(n-1,-1,-1):
+            prev=dp[:]
             for buy in range(2):
                 if buy:
-                    dp[ind][buy]=max(-prices[ind]+dp[ind+1][0],dp[ind+1][1])
+                    dp[buy]=max(-prices[ind]+prev[0],prev[1])
                 else:
-                    dp[ind][buy]=max(prices[ind]+dp[ind+1][1],dp[ind+1][0])
+                    dp[buy]=max(prices[ind]+prev[1],prev[0])
         # print(dp)
-        return dp[0][1]
+        return dp[1]

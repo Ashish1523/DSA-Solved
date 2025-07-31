d768670dc2e6aa9ff1e30ca2eb4779c27fe07634
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-07-31 22:45:26 +0530

    Time: 2173 ms (5.01%) | Memory: 83 MB (15.85%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -3,21 +3,32 @@ class Solution:
         maxProfit=0
         n=len(prices)
         dp=[[[-1 for _ in range(2+1)] for _ in range(2)] for _ in range(n)]
-        # print(dp)
-        def dfs(ind,buy,cap):
-            maxProfit=0
-            if cap==0:
-                return 0
-            if ind==n:
-                return 0
-            if dp[ind][buy][cap]!=-1:
-                return dp[ind][buy][cap]
-            if buy:
-                dp[ind][buy][cap]=max(-prices[ind]+dfs(ind+1,0,cap),dfs(ind+1,1,cap))
-                maxProfit=dp[ind][buy][cap]
-            else:
-                dp[ind][buy][cap]=max(prices[ind]+dfs(ind+1,1,cap-1),dfs(ind+1,0,cap))
-                maxProfit=dp[ind][buy][cap]
-            return maxProfit
+        # def dfs(ind,buy,cap):
+        #     maxProfit=0
+        #     if cap==0:
+        #         return 0
+        #     if ind==n:
+        #         return 0
+        #     if dp[ind][buy][cap]!=-1:
+        #         return dp[ind][buy][cap]
+        #     if buy:
+        #         dp[ind][buy][cap]=max(-prices[ind]+dfs(ind+1,0,cap),dfs(ind+1,1,cap))
+        #         maxProfit=dp[ind][buy][cap]
+        #     else:
+        #         dp[ind][buy][cap]=max(prices[ind]+dfs(ind+1,1,cap-1),dfs(ind+1,0,cap))
+        #         maxProfit=dp[ind][buy][cap]
+        #     return maxProfit
         
-        return dfs(0,1,2)
+        # return dfs(0,1,2)
+        dp=[[[0 for _ in range(2+1)] for _ in range(2)] for _ in range(n+1)]
+        for ind in range(n-1,-1,-1):
+            for buy in range(2):
+                for cap in range(1,3):
+                    if buy:
+                        dp[ind][buy][cap]=max(-prices[ind]+dp[ind+1][0][cap],dp[ind+1][1][cap])
+                        # maxProfit=dp[ind][buy][cap]
+                    else:
+                        dp[ind][buy][cap]=max(prices[ind]+dp[ind+1][1][cap-1],dp[ind+1][0][cap])
+                    #     maxProfit=dp[ind][buy][cap]
+                    # return maxProfit
+        return dp[0][1][2]

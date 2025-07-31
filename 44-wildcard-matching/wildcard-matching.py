194fc8dd0578a6e1c2a0e57056b082880115c4ef
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-07-31 16:40:00 +0530

    Time: 433 ms (27.07%) | Memory: 26.3 MB (41.54%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.py
+++ 44-wildcard-matching/wildcard-matching.py
@@ -2,26 +2,43 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         s1=len(s)
         s2=len(p)
-        dp=[[-1 for _ in range(s2)] for _ in range(s1)]
-        def dfs(ind1,ind2):
-            if ind1<0 and ind2<0:
-                return True
-            if ind2<0:
-                return False
-            if ind1<0:
-                # print(p[:ind2+1])
-                for i in range(ind2+1):
-                    if p[i]!='*':
-                        return False
-                return True
-            if dp[ind1][ind2]!=-1:
-                return dp[ind1][ind2]
-            if s[ind1]==p[ind2] or p[ind2]=="?":
-                dp[ind1][ind2]=dfs(ind1-1,ind2-1)
-                return dp[ind1][ind2]
-            if p[ind2]=="*":
-                dp[ind1][ind2]=dfs(ind1-1,ind2) or dfs(ind1,ind2-1)
-                return dp[ind1][ind2]
-            dp[ind1][ind2]=False
-            return dp[ind1][ind2]
-        return dfs(s1-1,s2-1)
+        # dp=[[-1 for _ in range(s2+1)] for _ in range(s1+1)]
+        # def dfs(ind1,ind2):
+        #     if ind1==0 and ind2==0:
+        #         return True
+        #     if ind2==0:
+        #         return False
+        #     if ind1==0:
+        #         for i in range(ind2):
+        #             if p[i]!='*':
+        #                 return False
+        #         return True
+        #     if dp[ind1][ind2]!=-1:
+        #         return dp[ind1][ind2]
+        #     if s[ind1-1]==p[ind2-1] or p[ind2-1]=="?":
+        #         dp[ind1][ind2]=dfs(ind1-1,ind2-1)
+        #         return dp[ind1][ind2]
+        #     if p[ind2-1]=="*":
+        #         dp[ind1][ind2]=dfs(ind1-1,ind2) or dfs(ind1,ind2-1)
+        #         return dp[ind1][ind2]
+        #     dp[ind1][ind2]=False
+        #     return dp[ind1][ind2]
+        # return dfs(s1,s2)
+
+        dp=[[False for _ in range(s2+1)] for _ in range(s1+1)]
+        dp[0][0]=True
+        for j in range(1, s2+1):
+            if p[j-1] == '*':
+                dp[0][j] = dp[0][j-1]
+            else:
+                break
+
+        for ind1 in range(1,s1+1):
+            for ind2 in range(1,s2+1):
+                if s[ind1-1]==p[ind2-1] or p[ind2-1]=="?":
+                    dp[ind1][ind2]=dp[ind1-1][ind2-1]
+                elif p[ind2-1]=="*":
+                    dp[ind1][ind2]=dp[ind1-1][ind2] or dp[ind1][ind2-1]
+                else:
+                    dp[ind1][ind2]=False
+        return dp[s1][s2]

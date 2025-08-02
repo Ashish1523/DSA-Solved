f51bb084334eacd7376af6a77d2a621709ddeef4
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-08-02 12:59:38 +0530

    Time: 367 ms (27.11%) | Memory: 28.4 MB (27.74%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,15 +1,24 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         n=len(prices)
-        dp=[[-1 for _ in range(2)] for _ in range(n)]
-        def dfs(i,buy):
-            if i==n:
-                return 0
-            if dp[i][buy]!=-1:
-                return dp[i][buy]
-            if buy:
-                dp[i][buy]=max(-prices[i]+dfs(i+1,0),dfs(i+1,1))
-            else:
-                dp[i][buy]=max(prices[i]-fee+dfs(i+1,1),dfs(i+1,0))
-            return dp[i][buy]
-        return dfs(0,1)
+        # dp=[[-1 for _ in range(2)] for _ in range(n)]
+        # def dfs(i,buy):
+        #     if i==n:
+        #         return 0
+        #     if dp[i][buy]!=-1:
+        #         return dp[i][buy]
+        #     if buy:
+        #         dp[i][buy]=max(-prices[i]+dfs(i+1,0),dfs(i+1,1))
+        #     else:
+        #         dp[i][buy]=max(prices[i]-fee+dfs(i+1,1),dfs(i+1,0))
+        #     return dp[i][buy]
+        # return dfs(0,1)
+
+        dp=[[0 for _ in range(2)] for _ in range(n+1)]
+        for i in range(n-1,-1,-1):
+            for buy in range(2):
+                if buy:
+                    dp[i][buy]=max(-prices[i]+dp[i+1][0],dp[i+1][1])
+                else:
+                    dp[i][buy]=max(prices[i]-fee+dp[i+1][1],dp[i+1][0])
+        return dp[0][1]

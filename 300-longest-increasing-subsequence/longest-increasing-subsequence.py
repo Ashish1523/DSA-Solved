4674e75d2df8ac9aa4d556acd2e1613e3abc27c2
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-08-02 16:09:54 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (19.39%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -6,7 +6,7 @@ class Solution:
         #         if nums[i]<nums[j]:
         #             LIS[i]=max(LIS[i],1+LIS[j])
         # return max(LIS)
-        n=len(nums)
+        # n=len(nums)
         # dp=[[-1 for _ in range(n+1)] for _ in range(n)]
         # def dfs(ind,prev):
         #     if ind==n:
@@ -21,13 +21,22 @@ class Solution:
         #     return dp[ind][prev+1]
         # return dfs(0,-1)
 
-        dp=[0 for _ in range(n+1)]
-        for ind in range(n-1,-1,-1):
-            prev_r=dp[:]
-            for prev in range(ind-1,-2,-1):
-                notPick=prev_r[prev+1]
-                pick=0
-                if prev==-1 or nums[ind]>nums[prev]:
-                    pick=1+prev_r[ind+1]
-                dp[prev+1]=max(pick,notPick)
-        return dp[prev+1]
+        # dp=[0]*(n+1)
+        # for ind in range(n-1,-1,-1):
+        #     for prev in range(-1,ind):
+        #         notPick=dp[prev+1]
+        #         pick=0
+        #         if prev==-1 or nums[ind]>nums[prev]:
+        #             pick=1+dp[ind+1]
+        #         dp[prev+1]=max(pick,notPick)
+        # return dp[prev+1]
+
+        res = []
+
+        for num in nums:
+            if not res or num >res[-1]:
+                res.append(num)
+            else:
+                res[bisect.bisect_left(res, num)] = num
+
+        return len(res)

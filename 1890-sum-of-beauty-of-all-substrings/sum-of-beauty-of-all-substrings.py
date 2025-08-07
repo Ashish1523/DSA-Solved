8e58e12e26c602d1aeb221bd792e5390b0ef27e9
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-08-07 14:43:49 +0530

    Time: 1201 ms (91.69%) | Memory: 17.7 MB (80.95%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.py
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.py
@@ -2,17 +2,10 @@ class Solution:
     def beautySum(self, s: str) -> int:
         n=len(s)
         sum1=0
-        def find(freq):
-            maxy=0
-            mini=501
-            for i in freq:
-                maxy=max(i,maxy)
-                mini=min(mini,i) if i!=0 else mini
-            return maxy-mini
         for i in range(n):
-            freq=[0]*26
+            hashy={}
             for j in range(i,n):
-                freq[ord(s[j])-ord('a')]+=1
-                beauty=find(freq)
+                hashy[s[j]]=hashy.get(s[j],0)+1
+                beauty=max(hashy.values())-min(hashy.values())
                 sum1+=beauty
         return sum1

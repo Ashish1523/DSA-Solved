9c259fe9775eed0db7a878f93dc67664fa6ad5e9
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-08-12 20:54:24 +0530

    Time: 288 ms (79.65%) | Memory: 18 MB (62.50%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.py
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.py
@@ -1,16 +1,18 @@
 class Solution:
     def numberOfWays(self, n: int, x: int) -> int:
-        MOD = (10**9)+7
-        @lru_cache(None)
-        def func(idx,sums):
-            nonlocal x , n
-            if idx == n+1:
-                return 1 if sums==0 else 0
-            not_take = func(idx+1,sums)
-            temp = idx**x
-            take = 0
-            if sums >= temp:
-                take += func(idx+1,sums-temp)
-            return (take+not_take)%MOD
+        mod=10**9+7
+        dp=[0]*(n+1)
+        dp[0]=1
+        powers=[]
+        i=1
+        while(True):
+            power=pow(i,x)
+            if power>n:
+                break
+            powers.append(power)
+            i+=1
+        for p in powers:
+            for j in range(n,p-1,-1):
+                dp[j]=(dp[j]+dp[j-p])%mod
 
-        return func(1,n)%MOD
+        return dp[n]

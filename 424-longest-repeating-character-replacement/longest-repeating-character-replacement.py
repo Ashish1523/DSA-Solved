612ee385194f99544d8ef98b0194c43742a10cea
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-08-15 13:12:59 +0530

    Time: 115 ms (73.46%) | Memory: 18 MB (87.13%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -5,9 +5,11 @@ class Solution:
         l=0
         ans=0
         mapy=[0]*26
+        maxf=0
         while r<n:
             mapy[ord(s[r])-ord('A')]+=1
-            if (r-l+1)-max(mapy)>k:
+            maxf=max(maxf,mapy[ord(s[r])-ord('A')])
+            if (r-l+1)-maxf>k:
                 mapy[ord(s[l])-ord('A')]-=1
                 l+=1
             ans=max(ans,r-l+1)

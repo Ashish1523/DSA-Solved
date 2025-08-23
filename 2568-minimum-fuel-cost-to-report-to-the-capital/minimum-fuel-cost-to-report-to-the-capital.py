617c3a91453969af5afd2cf98a3d2a0269fa2c3b
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-08-23 22:17:56 +0530

    Time: 231 ms (58.98%) | Memory: 83.5 MB (18.98%) - LeetSync

---
+++ 2568-minimum-fuel-cost-to-report-to-the-capital/minimum-fuel-cost-to-report-to-the-capital.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
+        adj=defaultdict(list)
+        for src,dest in roads:
+            adj[src].append(dest)
+            adj[dest].append(src)
+
+        def dfs(node,parent):
+            nonlocal res
+            passengers=0
+            for child in adj[node]:
+                if child !=parent:
+                    p=dfs(child,node)
+                    passengers+=p
+                    res+=int(ceil(p/seats))
+            return passengers+1
+        
+        res=0
+        dfs(0,-1)
+        return res
+        

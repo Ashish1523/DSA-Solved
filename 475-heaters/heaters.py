23fb44c4d08a25c8285e77d83f63d10e4d04c833
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-09-03 19:34:46 +0530

    Time: 87 ms (39.71%) | Memory: 20.6 MB (42.48%) - LeetSync

---
+++ 475-heaters/heaters.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        houses.sort()
+        heaters.sort()
+        ans=0
+
+        for house in houses:
+            left=0
+            right=len(heaters)-1
+            while left<right:
+                mid=(left+right)//2
+                if heaters[mid] < house:
+                    left=mid+1
+                else:
+                    right=mid
+            dist=abs(heaters[left]-house)
+            if left>0:
+                dist=min(dist,abs(heaters[left-1]-house))
+            ans=max(ans,dist)
+        return ans

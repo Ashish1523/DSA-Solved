313d3d3da60eb629610d7eb1afe4cb1034c53b63
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-09-05 11:05:02 +0530

    Time: 6072 ms (6.73%) | Memory: 19.8 MB (6.78%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n=len(nums)
+        dp=[-1]*n
+        def dfs(i):
+            if i>=n:
+                return float("inf")
+            if i==n-1:
+                return 0
+            if nums[i]==0:
+                return float("inf")
+            if dp[i]!=-1:
+                return dp[i]
+            steps=float("inf")
+            for j in range(nums[i],0,-1):
+                steps=min(steps,1+dfs(i+j))
+            dp[i]=steps
+            return steps
+        ans=dfs(0)
+        return ans
+
+            

9aa714e20fc646623270716d0999d66e2317cc85
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-09-12 19:08:50 +0530

    Time: 23 ms (21.19%) | Memory: 20.1 MB (10.48%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -2,14 +2,16 @@ class Solution:
     def candy(self, ratings: List[int]) -> int:
         n=len(ratings)
         left=[1]*n
+        right=[1]*n
         for i in range(1,n):
             if ratings[i]>ratings[i-1]:
                 left[i]+=left[i-1]
-        # print(left)
-        right=[1]*n
-        for i in range(n-2,-1,-1):
-            if ratings[i]>ratings[i+1]:
-                right[i]+=right[i+1]
+            if ratings[n-i-1]>ratings[n-i]:
+                right[n-i-1]+=right[n-i]
+        
+        # for i in range(n-2,-1,-1):
+        #     if ratings[i]>ratings[i+1]:
+        #         right[i]+=right[i+1]
 
         # print(right)
         ans=0

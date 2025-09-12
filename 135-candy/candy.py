44d4b8e5f2e96e33d813cf1be648baec1fd23abb
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-09-12 19:06:43 +0530

    Time: 23 ms (21.19%) | Memory: 20.1 MB (10.48%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -5,13 +5,13 @@ class Solution:
         for i in range(1,n):
             if ratings[i]>ratings[i-1]:
                 left[i]+=left[i-1]
-        print(left)
+        # print(left)
         right=[1]*n
         for i in range(n-2,-1,-1):
             if ratings[i]>ratings[i+1]:
                 right[i]+=right[i+1]
 
-        print(right)
+        # print(right)
         ans=0
         for i in range(n):
             ans+=max(left[i],right[i])

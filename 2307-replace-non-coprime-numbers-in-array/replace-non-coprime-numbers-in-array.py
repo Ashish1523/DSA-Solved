a1a88c29d2d1da3caa86108ca71ca8298e0ae15a
Ashish Chandra K. <ashish.chandra022@gmail.com>
2025-09-16 23:16:20 +0530

    Time: 163 ms (35.87%) | Memory: 31.6 MB (59.78%) - LeetSync

--- 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.py
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.py
@@ -1,16 +1,16 @@
 class Solution:
     def replaceNonCoprimes(self, nums: List[int]) -> List[int]:
         stack=[]
-        def gcd(num1,num2):
-            if num2==0:
-                # print("gcd",num1)
-                return num1
-            return gcd(num2,num1%num2)
+        # def gcd(num1,num2):
+        #     if num2==0:
+        #         # print("gcd",num1)
+        #         return num1
+        #     return gcd(num2,num1%num2)
         stack
         for num in nums:
             stack.append(num)
             while len(stack)>1:
-                val=gcd(stack[-1],stack[-2])
+                val=math.gcd(stack[-1],stack[-2])
                 if val!=1:
                     num1=stack.pop()
                     num2=stack.pop()
